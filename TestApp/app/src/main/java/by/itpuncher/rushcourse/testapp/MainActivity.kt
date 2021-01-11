98b55f53b8646d6cfda3d074c4f0dc4a3a40013b
diana.sekatskaya@gmail.com <diana.sekatskaya@gmail.com>
2021-01-11 09:53:40 +0300

    Lesson 7. Show progress whilst data is loading

--- TestApp/app/src/main/java/by/itpuncher/rushcourse/testapp/MainActivity.kt
+++ TestApp/app/src/main/java/by/itpuncher/rushcourse/testapp/MainActivity.kt
@@ -3,19 +3,25 @@ package by.itpuncher.rushcourse.testapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.concurrent.thread
 
 const val CREATE_NOTE_CODE = 101
 
 class MainActivity : AppCompatActivity() {
 
     val adapter = RecordsAdapter()
+    lateinit var recyclerView: RecyclerView
+    lateinit var progress: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progress = findViewById(R.id.progress)
         supportActionBar?.hide()
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener {
@@ -23,16 +29,29 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(intent, CREATE_NOTE_CODE)
         }
 
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView = findViewById(R.id.recycler_view)
         val lm = LinearLayoutManager(this)
         recyclerView.layoutManager = lm
         recyclerView.adapter = adapter
 
-        val data = StorageUtil.loadFromFile(this)
-        val records: ArrayList<Record> = arrayListOf()
-        data.forEach { records.add(Record(it.first.toString(), it.second)) }
+        loadData()
+    }
 
-        adapter.setData(records)
+    private fun loadData() {
+        recyclerView.visibility = View.GONE
+        progress.visibility = View.VISIBLE
+        thread(start = true) {
+            val data = StorageUtil.loadFromFile(this)
+            val records: ArrayList<Record> = arrayListOf()
+            data.forEach { records.add(Record.build(it.first, it.second)) }
+            runOnUiThread {
+                progress.visibility = View.GONE
+                recyclerView.visibility = View.VISIBLE
+                adapter.setData(records)
+            }
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == CREATE_NOTE_CODE) {
             val record = data?.getSerializableExtra(RECORD)
